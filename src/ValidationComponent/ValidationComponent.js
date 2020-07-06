bda19cdf8034946623055ab8503ba0917bf88d0a
LeanneDo <49956780+leannedo@users.noreply.github.com>
2020-07-06 12:40:43 +0300

    Update ValidationComponent.js

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -3,9 +3,6 @@ import React from 'react';
 const ValidationComponent = ({ text }) => {
 
     const validateLength = (len) => {
-        // return len === 0 ? 0 : len > 5 ? 1 : -1;
-        // the case in which we have 3 conditions <5, >5, =0
-
         return len === 0 ? 0 : len < 5 ? -1 : 1;
     }
 
